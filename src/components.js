217557cdbadc806065aac7680e0049f55c462452
rice2007 <rice2007@gmail.com>
2015-04-16 12:43:25 -0500

    movement

--- src/components.js
+++ src/components.js
@@ -191,30 +191,35 @@ Crafty.c('PlayerCharacter', {
 //            Crafty.trigger("KeyDown", lastKeyEvent)
         });
 
-
-
-
         this.bind('NewDirection', function (data) {
             if (data.x > 0) {
                 this.animate('PlayerMovingRight', -1);
                 lastKeyEvent = {
                     key: Crafty.keys.RIGHT_ARROW
                 };
+                Crafty.rg.move('w', 20);
+                Crafty.bg.move('w', 20);
             } else if (data.x < 0) {
                 this.animate('PlayerMovingLeft', -1);
                 lastKeyEvent = {
                     key: Crafty.keys.LEFT_ARROW
                 };
+                Crafty.rg.move('s', 20);
+                Crafty.bg.move('n', 20);
             } else if (data.y > 0) {
                 this.animate('PlayerMovingDown', -1);
                 lastKeyEvent = {
                     key: Crafty.keys.DOWN_ARROW
                 };
+                Crafty.rg.move('e', 20);
+                Crafty.bg.move('s', 20);
             } else if (data.y < 0) {
                 this.animate('PlayerMovingUp', -1);
                 lastKeyEvent = {
                     key: Crafty.keys.UP_ARROW
                 };
+                Crafty.rg.move('n', 20);
+                Crafty.bg.move('e', 20);
             } else {
                 this.pauseAnimation();
             }
@@ -249,89 +254,6 @@ Crafty.c('PlayerCharacter', {
     }
 });
 
-Crafty.c("Slide", {
-    init: function() {
-      this._stepFrames = 5;
-      this._tileSize = 32;
-      this._moving = false;
-      this._vx = 0; this._destX = 0; this._sourceX = 0;
-      this._vy = 0; this._destY = 0; this._sourceY = 0;
-      this._frames = 0;
-
-      this.bind("Slide", function(direction) {
-        // Don't continue to slide if we're already moving
-        if(this._moving) return false;
-        this._moving = true;
-
-        // Let's keep our pre-movement location
-        // Hey, Maybe we'll need it later :)
-        this._sourceX = this.x;
-        this._sourceY = this.y;
-
-        // Figure out our destination
-        this._destX = this.x + direction[0] * 32;
-        this._destY = this.y + direction[1] * 32;
-
-        // Get our x and y velocity
-        this._vx = direction[0] * this._tileSize / this._stepFrames;
-        this._vy = direction[1] * this._tileSize / this._stepFrames;
-
-        this._frames = this._stepFrames;
-      }).bind("EnterFrame",function(e) {
-        if(!this._moving) return false;
-
-        // If we'removing, update our position by our per-frame velocity
-        this.x += this._vx;
-        this.y += this._vy;
-        this._frames--;
-
-        if(this._frames == 0) {
-          // If we've run out of frames,
-          // move us to our destination to avoid rounding errors.
-          this._moving = false;
-          this.x = this._destX;
-          this.y = this._destY;
-        }
-        this.trigger('Moved', {x: this.x, y: this.y});
-      });
-
-    }, 
-    slideFrames: function(frames) { 
-       this._stepFrames = frames;
-    },
-
-    // A function we'll use later to 
-    // cancel our movement and send us back to where we started
-    cancelSlide: function() {
-      this.x = this._sourceX;
-      this.y = this._sourceY;
-      this._moving = false;
-    }
-  });
-
-Crafty.c("AI",{
-  _directions:  ['s', 'e', 'w', 'n'],
-  init: function() {
-    this._moveChance = 0.5;
-    this.requires('Slide');
-
-    this.bind("Turn",function() {
-      if(Math.random() < this._moveChance) {
-        this.trigger("Slide", this._randomDirection());
-      }
-    });
-  },
-
-  moveChance: function(val) {
-    this._moveChance = val;
-
-  },
-  _randomDirection: function() {
-    return this._directions[Math.floor(Math.random()*4)];
-  }
-
-});
-
 // A pellet is a tile on the grid that the PC must visit in order to win the game
 Crafty.c('Pellet', {
     init: function () {
@@ -348,16 +270,20 @@ Crafty.c('Pellet', {
 
 Crafty.c('Ghost', {
     init: function() {
-        this.requires('Actor');
+        this.requires('Actor, Collision');
+        this.stopOnSolids()
     },
 
-    move: function() {
+    stopOnSolids: function () {
+        this.onHit('Solid', this.stopMovement);
+        return this;
+    },
 
-    }
-});
-
-Crafty.c('RedMove', {
-    init: function() {
-        Crafty.rg.move('w', 20);
-    }
+    stopMovement: function () {
+        this._speed = 0;
+        if (this._movement) {
+            this.x -= this._movement.x;
+            this.y -= this._movement.y;
+        }
+    },
 });
--- src/scenes.js
+++ src/scenes.js
@@ -6,7 +6,7 @@
 // -------------
 // Runs the core gameplay loop
 Crafty.scene('Game', function () {
-        
+    
     // A 2D array to keep track of all occupied tiles
     this.occupied = new Array(Game.map_grid.width);
     for (var i = 0; i < Game.map_grid.width; i++) {
